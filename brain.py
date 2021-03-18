0447ec36f3821c174bc39c77ab65a75555e047a3
18behrens <8behrens@informatik.uni-hamburg.de>
2021-03-18 01:34:24 +0100

    debug log

--- brain.py
+++ brain.py
@@ -34,4 +34,4 @@ app.add_routes([web.get('/', handle_index),
                 ])
 
 if __name__ == '__main__':
-    web.run_app(app, host="0.0.0.0")
+    web.run_app(app)
--- my_log.py
+++ my_log.py
@@ -1,2 +1,4 @@
 import logging
+logging.basicConfig(format=logging.BASIC_FORMAT)
 my_log = logging.getLogger()
+my_log.setLevel(logging.DEBUG)
