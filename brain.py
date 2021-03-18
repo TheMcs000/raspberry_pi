e18e3455d9f93caa5f06a9c18932adf93fc53915
18behrens <8behrens@informatik.uni-hamburg.de>
2021-03-18 01:47:34 +0100

    explicit in and out routes

--- brain.py
+++ brain.py
@@ -18,19 +18,22 @@ async def handle_index(request):
     return web.Response(text="This is the index page")
 
 
-async def handle_barrier(request):
-    name = request.match_info.get('direction', "in")  # possible values: in, out
-    direction_is_in = name == "in"
-    if direction_is_in:
-        print("TODO: DIRECTION IN")  # todo: direction in
-    else:  # direction is out
-        save_say(settings.SAY_SHOULD_TURN_LIGHT_OFF)
+async def handle_barrier_in(request):
+    print("TODO: DIRECTION IN")  # todo: direction in
+    return web.Response(text="OK")
+
+
+async def handle_barrier_out(request):
+    print("TODO: DIRECTION OUT")  # todo: direction out
+    save_say(settings.SAY_SHOULD_TURN_LIGHT_OFF)
     return web.Response(text="OK")
 
 
 app = web.Application()
 app.add_routes([web.get('/', handle_index),
-                web.get('/barrier/{direction}', handle_barrier),  # possible values: in, out
+                # web.get('/barrier/{direction}', handle_barrier),  # possible values: in, out
+                web.get('/barrier/in', handle_barrier_in),
+                web.get('/barrier/out', handle_barrier_out),
                 ])
 
 if __name__ == '__main__':
