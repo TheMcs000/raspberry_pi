259dbefd7bdcc6d0b37969a1acc483086426e349
Markus Behrens <33362659+TheMcs000@users.noreply.github.com>
2022-02-05 16:31:29 +0100

    Update my_log.py

--- python/my_log.py
+++ python/my_log.py
@@ -1,4 +1,11 @@
 import logging
-logging.basicConfig(format=logging.BASIC_FORMAT)
+from logging.handlers import RotatingFileHandler
+from settings import LOG_FILE
+
+logging.basicConfig(
+        handlers=[logging.StreamHandler(),
+                  RotatingFileHandler(LOG_FILE, maxBytes=100000, backupCount=10)],
+        level=logging.DEBUG,
+        format="[%(asctime)s] %(levelname)s [%(pathname)s -> %(funcName)s:%(lineno)d] %(message)s",
+        datefmt='%Y-%m-%dT%H:%M:%S')
 my_log = logging.getLogger()
-my_log.setLevel(logging.DEBUG)
